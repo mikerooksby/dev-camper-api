48a3a60546cd216cf8a85173a1a5564ca90e26e7
Mike Rooksby <mikerooksby@gmail.com>
2022-10-24 12:13:02 -0400

    Add MongoDB Connection, Sample Data, Organize to MVC

---
+++ controllers/bootcampControllers.js
@@ -0,0 +1,40 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+};
+
+// @desc    Get single bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Show bootcamp ${req.params.id}` });
+};
+
+// @desc    Create a single bootcamp
+// @route   POST /api/v1/bootcamps/
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(201).json({ success: true, message: 'Created new bootcamp' });
+};
+
+// @desc    Update a single bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc    Delete a single bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -2,22 +2,43 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const colors = require('colors');
-require('dotenv').config({ path: './config/.env' });
+const connectDB = require('./config/db');
 
+// Load Env Vars
+require('dotenv').config({ path: './config/.env' });
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 8000;
 
+// Connect to Database
+connectDB();
+
+// Route Files
+const bootcampRoutes = require('./routes/bootcampRoutes');
+
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-app.listen(PORT, () => {
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcampRoutes);
+
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode at http://${HOST}:${PORT}`
       .brightWhite.bold.underline
   );
+});
+
+// Handle Unhandled Promise Rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.underline.bold);
+  server.close(() => {
+    process.exit(1);
+  });
 });
