516a2b5972a6432b7029c6e8d72fbb42586b358d
Mike Rooksby <mikerooksby@gmail.com>
2022-10-25 15:16:24 -0400

    Update seed script to handle courses

--- seed.js
+++ seed.js
@@ -7,6 +7,7 @@ require('dotenv').config({ path: './config/.env' });
 
 // Load Models
 const Bootcamp = require('./models/bootcampModel');
+const Course = require('./models/courseModel');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -15,11 +16,15 @@ mongoose.connect(process.env.MONGO_URI);
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Seed DB
 const seedDB = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -30,6 +35,7 @@ const seedDB = async () => {
 // Purge DB
 const purgeDB = async () => {
   try {
+    await Course.deleteMany();
     await Bootcamp.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
