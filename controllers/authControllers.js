7c36109477f72268707cfb27344c550497a4f3d4
Mike Rooksby <mikerooksby@gmail.com>
2022-11-09 15:39:12 -0500

    Send JWT in cookie

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create Token
-  const token = user.getSignedToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login User
@@ -47,8 +44,26 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
-  // Create Token
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie & send response
+const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const colors = require('colors');
 const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/db');
@@ -24,6 +25,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(fileUpload());
